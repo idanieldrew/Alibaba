3056b78590d31fde4867867d38de28c50e575565
idanieldrew <danielgia35@gmail.com>
2022-01-29 15:26:03 +0330

    Add Plane Resource

---
+++ Modules/Airplane/Http/Resources/v1/AirplaneResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Module\Airplane\Http\Resources\v1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AirplaneResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'details' => $this->details,
+            'code' => $this->code,
+            'floor' => $this->floor,
+            'created_at' => $this->created_at,
+        ];
+    }
+}
