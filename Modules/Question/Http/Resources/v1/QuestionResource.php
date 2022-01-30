a0f56c9ff70dfc08f2cc6e3b8de090f127ed255e
idanieldrew <danielgia35@gmail.com>
2022-01-30 15:23:30 +0330

    Add resource to Question

---
+++ Modules/Question/Http/Resources/v1/QuestionResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Module\Airplane\Http\Resources\v1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QuestionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'question' => $this->question,
+            'answer' => $this->answer
+        ];
+    }
+}
