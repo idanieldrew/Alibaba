ba29be32cf7ebfe4e0563d969e90d5bfd61eb537
idanieldrew <danielgia35@gmail.com>
2022-01-30 15:21:08 +0330

    Add resource to Passenger

---
+++ Modules/Passenger/Http/Resources/v1/PassengerResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Module\Airplane\Http\Resources\v1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PassengerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'firstName' => $this->firstName,
+            'lastName' => $this->lastName,
+            'gender' => $this->gender,
+            'identification_code' => $this->identification_code,
+            'birthday' => $this->birthday,
+        ];
+    }
+}
