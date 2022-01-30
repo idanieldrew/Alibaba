f7ba9a00270dadbf97c917a1ccfd9e671dd21660
idanieldrew <danielgia35@gmail.com>
2022-01-30 15:28:06 +0330

    Add resource to User

---
+++ Modules/User/Http/Resources/v1/UserResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Module\Airplane\Http\Resources\v1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'phone' => $this->phone,
+            'email' => $this->email,
+            'type' => $this->type
+        ];
+    }
+}
